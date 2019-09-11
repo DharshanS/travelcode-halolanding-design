e4ade0302eb70fded6e21c1d3c26241058067f92
Dharshan+2-at-988279413178 <cdharshans@gmail.com>
2019-09-11 10:56:43 +0530

    month change

--- assets/js/helo.js
+++ assets/js/helo.js
@@ -15,18 +15,18 @@ $(document).ready(function() {
   };
 
   const datsList = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC"
   ];
 
   var roomsList = [];
